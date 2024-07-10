607b6a27ed4ff21c490f888b0fbc68dab44ef065
Jan Gango <jangangolells@gmail.com>
2024-07-10 21:57:02 +0200

    Todo funciona. Ahora solo falta hacerlo bonito :)

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const Input = document.getElementById("Input");
 const Geo = document.getElementById("Geo");
 const CiudadGeo = document.getElementById("temperatura-nombre");
 const errores = document.getElementById("Error");
+const Pregunta = document.getElementById("Pregunta");
 
 BotonBuscar.addEventListener("click", () => {
   const ciudad = Input.value;
-  const url_ciudad = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=0f33901d085922ce186457a1a8080b62&units=metric`;
+  const url_ciudad = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=0f33901d085922ce186457a1a8080b62&units=metric&lang=es`;
   fetch(url_ciudad)
     .then((response) => {
       return response.json();
@@ -24,13 +25,16 @@ BotonBuscar.addEventListener("click", () => {
       let loc = data.name;
       CiudadGeo.textContent = loc;
       errores.textContent = ""
+
+      Pregunta.textContent = ""
     })
     .catch((error) => {
       console.log("Nombre de ciudad equivocado o no encontrado");
-      errores.textContent = "Nombre de ciudad equivocado o no encontrado";
+      errores.textContent = "Ciudad no encontrada o mal escrita. Revisa antes de enviar crack ;)";
       temperatura.textContent = "";
       descripción.textContent = "";
       CiudadGeo.textContent = "";
+      Pregunta.textContent = ""
     });
 });
 
@@ -41,7 +45,7 @@ Geo.addEventListener("click", () => {
     longitude = posicion.coords.longitude;
     latitude = posicion.coords.latitude;
 
-    const urlGeolocalización = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=0f33901d085922ce186457a1a8080b62`;
+    const urlGeolocalización = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=0f33901d085922ce186457a1a8080b62&units=metric&lang=es`;
     fetch(urlGeolocalización)
       .then((respuesta) => {
         return respuesta.json();
@@ -59,10 +63,12 @@ Geo.addEventListener("click", () => {
 
         let loc = data.name;
         CiudadGeo.textContent = loc;
+        Pregunta.textContent = ""
       })
       .catch((error) => {
-        console.log("Nombre de ciudad equivocado o no encontrado");
-        error.textContent = "Error"
+        console.log("Ciudad no encontrada o mal escrita. Revisa antes de enviar crack ;)");
+        error.textContent = "GPS desactivado o no encontrado. Intenta estar en un sitio visible para los satelites crack ;)"
+        Pregunta.textContent = ""
       });
   });
 });
